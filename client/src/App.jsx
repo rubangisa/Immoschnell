c2bd4945d214a8233e8f0ec842edc1a3875bef0d
didier <rubangisadidier@gmail.com>
2024-02-07 18:17:05 +0100

    Sign up page added to App.jsx for testing purpose

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import SignUp from './pages/signUp';
+import signUp from './components/pages/signUp';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
