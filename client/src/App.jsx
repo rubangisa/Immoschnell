68e0bf6874016499f6c8a9e981564fa53a41265e
Billie Jean Bowdery <billie-jean.bowdery@dci.education>
2024-02-08 11:55:32 +0100

    updated change mybooking

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import SignUp from './components/pages/signUp.jsx';
 import NavBar from './components/navbar';
+import MyBooking from './components/pages/myBookings';
 import './App.css';
 const App = () => {
   return (
     <div>
       <NavBar />
       <SignUp />
+      <MyBooking />
     </div>
   );
 }
--- client/src/components/pages/myBookings.jsx
+++ client/src/components/pages/myBookings.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 
+
 const MyBooking = () => {
   const [propertyData, setPropertyData] = useState([]);
 
