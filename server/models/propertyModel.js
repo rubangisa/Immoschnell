2be7ba44792c9f8f2daf5858216adef4bda6d5e1
Ilya <zubrik1984@yahoo.com>
2024-02-08 08:35:04 +0100

    branch reset

--- server/models/propertyModel.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const propertySchema = new Schema({
-    title: String,
-    description: String,
-    price: Number,
-});
-
-export default model("Property", propertySchema);
