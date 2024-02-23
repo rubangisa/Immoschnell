d56c4d379f35205c6dd29ff2cd06d586844bb0e2
Pavithra <pavithra.narayanan.kutty@gmail.com>
2024-02-23 09:28:29 +0100

    routing updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,25 @@
-import SignUp from './components/pages/signUp.jsx';
-import NavBar from './components/navbar.jsx';
-import MyBooking from './components/pages/myBookings.jsx';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+//import { useReducer, useState } from "react";
+import Home from "./components/pages/homePage.jsx";
+import SignUp from "./components/pages/signUp.jsx";
+import NavBar from "./components/navbar.jsx";
+import MyBooking from "./components/pages/myBookings.jsx";
+import "./App.css";
+import Payments from "./components/pages/payments.jsx";
 const App = () => {
   return (
     <div>
-      <NavBar/>
-      <SignUp />
-      <MyBooking />
+      <NavBar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/bookings" element={<MyBooking />} exact />
+        <Route path="/login" element={<SignUp />} />
+        {/* Add more routes as needed */}
+        {/* 404 Page Not Found */}
+        <Route render={() => <h2>Page not found</h2>} />
+      </Routes>
     </div>
   );
-}
+};
 export default App;
